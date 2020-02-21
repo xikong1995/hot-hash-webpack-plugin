efb97241c2a9da757ee378a717a8f038f366ae5b
zhoulei <zhouleiwit@163.com>
2020-02-21 14:08:31 +0800

    init webpack plugin

---
+++ index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const crypto = require('crypto');
+const fs = require('fs');
+
+class HotHashWebpackPlugin {
+    constructor(options) {
+        this.version = options && options.version;
+    }
+
+    apply(compiler) {
+        compiler.hooks.afterEmit.tapAsync('BuildHashWebpackPlugin', (compilation, callback) => {
+            const outputPath = compiler.options.output.path;
+            const buildJsonFile = path.resolve(outputPath, 'filesinfo.json');
+            const fileInfoList = [];
+            const assets = compilation.getAssets();
+            assets.forEach(asset => {
+                const filepath = path.resolve(outputPath, asset.name);
+                const content = fs.readFileSync(filepath);
+                const hash = crypto.createHash('sha256');
+                hash.update(content);
+                const hashContent = hash.digest('hex');
+                fileInfoList.push({
+                    path: asset.name,
+                    hash: hashContent,
+                    size: content.length
+                });
+            });
+            const json = {
+                lastBuildTDate: new Date().toLocaleString(),
+                fileInfoList,
+                version: this.version
+            };
+            const text = JSON.stringify(json);
+            compiler.outputFileSystem.writeFile(buildJsonFile, text, callback);
+        });
+    }
+}
+
+module.exports = HotHashWebpackPlugin;
